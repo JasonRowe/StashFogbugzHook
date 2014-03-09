960e17b28b3cc120459d29a31be47d05b31d08d6
JasonRowe <Jason.Rowe@gmail.com>
2014-03-09 15:01:49 -0500

    logging commit message

--- src/main/java/com/protolabs/FogbugzHook.java
+++ src/main/java/com/protolabs/FogbugzHook.java
@@ -1,15 +1,23 @@
 package com.protolabs;
 
+import java.util.List;
+import java.net.URL;
+import java.util.Collection;
+
+import com.atlassian.stash.content.Changeset;
+import com.atlassian.stash.content.ChangesetsBetweenRequest;
 import com.atlassian.stash.hook.repository.*;
 import com.atlassian.stash.repository.*;
 import com.atlassian.stash.setting.*;
 import com.atlassian.stash.commit.CommitService;
-
-import java.net.URL;
-import java.util.Collection;
+import com.atlassian.stash.util.Page;
+import com.atlassian.stash.util.PageRequest;
+import com.atlassian.stash.util.PageUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
 
 public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 
@@ -48,8 +56,37 @@ public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySe
                         log.info("RefChange toHash - {}", toHash);
 
                         //For each RefChange, use the fromHash and toHash to extract the individual changesets: https://developer.atlassian.com/static/javadoc/stash/latest/api/reference/com/atlassian/stash/repository/RefChange.html
-                        //Using CommitService.getChangesetsBetween stream a list of changesets from RefChange.getFromHash and RefChange.getToHash
-                        //For each of these changesets, validate the message as you need.
+
+                        ChangesetsBetweenRequest request = new ChangesetsBetweenRequest.Builder(repository)
+                        .exclude(fromHash)
+                        .include(toHash)
+                        .build();
+
+                        // TODO put page size on const
+                        PageRequest page_request = PageUtils.newRequest(0, 25);
+
+                        Collection<Changeset> changesetsForRefChange = Lists.newArrayList();
+                        Page<Changeset> page = null;
+
+                        // page through change sets and add to list
+                        while (page == null || !page.getIsLastPage()) {
+
+                            // Using CommitService.getChangesetsBetween get a list of changesets from RefChange.getFromHash and RefChange.getToHash
+                            page = commitService.getChangesetsBetween(request, page_request);
+
+                            for (Changeset changeset : page.getValues()){
+                                changesetsForRefChange.add(changeset);
+                            }
+
+                            page_request = page.getNextPageRequest();
+                        }
+
+                        // For each of these changesets, send Fogbugz request as needed.
+                        for (Changeset changeset : changesetsForRefChange) {
+                             String commitMessage = changeset.getMessage();
+                             log.info("CommitMessage - {}", commitMessage);
+                        }
+
                     }
                     new URL(url).openConnection().getInputStream().close();
                 } catch (Exception e) {
