8c620cd41aca653d712ce87496f28e992a89c1c3
JasonRowe <Jason.Rowe@gmail.com>
2014-03-09 03:08:38 -0500

    adding logging

--- src/main/java/com/protolabs/FogbugzHook.java
+++ src/main/java/com/protolabs/FogbugzHook.java
@@ -6,8 +6,13 @@ import com.atlassian.stash.setting.*;
 import java.net.URL;
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 
+    //https://confluence.atlassian.com/display/STASH/Stash+debug+logging
+    private static final Logger log = LoggerFactory.getLogger(FogbugzHook.class);
     /**
      * TODO - Loop through refChanges find last commit
      * TODO - Determine if FogbugzID is in commit message
@@ -16,11 +21,21 @@ public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySe
     @Override
     public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
         String url = context.getSettings().getString("url");
+
+        log.info("Starting postReceive for FogbugzHook");
+
         if (url != null) {
             try {
-                new URL(url).openConnection().getInputStream().close();
-            } catch (Exception e) {
-                e.printStackTrace();
+                    for (RefChange refChange : refChanges) {
+
+                        log.info("looking at refchange" + refChange.getType());
+                        //For each RefChange, use the fromHash and toHash to extract the individual changesets: https://developer.atlassian.com/static/javadoc/stash/latest/api/reference/com/atlassian/stash/repository/RefChange.html
+                        //Using HistoryService.getChangesetsBetween stream a list of changesets from RefChange.getFromHash and RefChange.getToHash
+                        //For each of these changesets, validate the message as you need.
+                    }
+                    new URL(url).openConnection().getInputStream().close();
+                } catch (Exception e) {
+                    e.printStackTrace();
             }
         }
     }
