3585c76ee276b58f7c735fec2403ab251162733b
JasonRowe <Jason.Rowe@gmail.com>
2014-03-10 21:43:09 -0500

    refactoring and error handling

--- src/main/java/com/protolabs/FogbugzHook.java
+++ src/main/java/com/protolabs/FogbugzHook.java
@@ -35,10 +35,11 @@ public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySe
         this.commitService = commitService;
     }
 
-    //https://confluence.atlassian.com/display/STASH/Stash+debug+logging
+    // More info on Stash logging:
+    // https://confluence.atlassian.com/display/STASH/Stash+debug+logging
     private static final Logger log = LoggerFactory.getLogger(FogbugzHook.class);
 
-    // Proof of concept needs major refactoring
+    // Proof of concept needs refactoring and unit tests
     @Override
     public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
         String url = context.getSettings().getString("url");
@@ -50,123 +51,62 @@ public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySe
         log.info("Configured URL {}", url);
         log.info("Configured Regex {}", regex);
 
-        Pattern bugzIDPattern = Pattern.compile(regex);
+        Pattern bugzIDPattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
-        if (url != null) {
-            try {
-                    for (RefChange refChange : refChanges) {
+        try {
+                for (RefChange refChange : refChanges) {
+                    log.info("handling refchange type {}", refChange.getType());
 
-                        log.info("handling refchange type {}", refChange.getType());
+                    String fromHash = refChange.getFromHash();
+                    log.info("RefChange fromHash - {}", fromHash);
 
-                        String fromHash = refChange.getFromHash();
-                        log.info("RefChange fromHash - {}", fromHash);
+                    String toHash = refChange.getToHash();
+                    log.info("RefChange toHash - {}", toHash);
 
-                        String toHash = refChange.getToHash();
-                        log.info("RefChange toHash - {}", toHash);
+                    Collection<Changeset> changesetsForRefChange = extractChangesets(fromHash, toHash, repository);
 
-                        //For each RefChange, use the fromHash and toHash to extract the individual changesets: https://developer.atlassian.com/static/javadoc/stash/latest/api/reference/com/atlassian/stash/repository/RefChange.html
-                        ChangesetsBetweenRequest request = new ChangesetsBetweenRequest.Builder(repository)
-                            .exclude(fromHash)
-                            .include(toHash)
-                            .build();
+                    for (Changeset changeset : changesetsForRefChange) {
+                        String commitMessage = changeset.getMessage();
 
-                        // Setup paging for request
-                        PageRequest page_request = PageUtils.newRequest(0, 25);
+                        log.info("CommitMessage - {}", commitMessage);
 
-                        Collection<Changeset> changesetsForRefChange = Lists.newArrayList();
-                        Page<Changeset> page = null;
+                        Matcher bugzIDMatcher = bugzIDPattern.matcher(commitMessage);
+                        
+                        // For each of these changesets, send Fogbugz request as needed.
+                        while (bugzIDMatcher.find()){
+                            String bugzID = bugzIDMatcher.group(1);
+                            log.info("BugzID Found {}", bugzID);
 
-                        // page through change sets and add to list
-                        while (page == null || !page.getIsLastPage()) {
+                            String parentID = "";
 
-                            // Using CommitService.getChangesetsBetween get a list of changesets from RefChange.getFromHash and RefChange.getToHash
-                            page = commitService.getChangesetsBetween(request, page_request);
-
-                            for (Changeset changeset : page.getValues()){
-                                changesetsForRefChange.add(changeset);
+                            // Parse out the old/previous ID
+                            if (!changeset.getParents().isEmpty()) {
+                                MinimalChangeset parent = changeset.getParents().iterator().next();
+                                parentID = parent.getDisplayId();
                             }
 
-                            page_request = page.getNextPageRequest();
-                        }
+                            log.info("ParentID {}", parentID);
 
-                        //Use Id's to get detailed and post to Fogbugz
-                        for (Changeset changeset : changesetsForRefChange) {
-                            String commitMessage = changeset.getMessage();
+                            // Parse out the new/current file SHA1
+                            String currentID = changeset.getDisplayId();
 
-                            log.info("CommitMessage - {}", commitMessage);
+                            log.info("CurrentID {}", currentID);
 
-                            Matcher bugzIDMatcher = bugzIDPattern.matcher(commitMessage);
-                            
-                            // For each of these changesets, send Fogbugz request as needed.
-                            while (bugzIDMatcher.find()){
-                                String bugzID = bugzIDMatcher.group(1);
-                                log.info("BugzID Found {}", bugzID);
+                            String changesetId = changeset.getId();
+                            log.info("ChangesetID {}", changesetId);
 
-                                String parentID = "";
+                            String fileName = getFileName(changesetId, repository);
 
-                                // Parse out the old/previous ID
-                                if (!changeset.getParents().isEmpty()) {
-                                    MinimalChangeset parent = changeset.getParents().iterator().next();
-                                    parentID = parent.getDisplayId();
-                                }
+                            log.info("FileName {}", fileName);
 
-                                log.info("ParentID {}", parentID);
-
-                                // Parse out the new/current file SHA1
-                                String currentID = changeset.getDisplayId();
-
-                                log.info("CurrentID {}", currentID);
-
-                                String changesetId = changeset.getId();
-                                log.info("ChangesetID {}", changesetId);
-
-                                // Create detailed request to get filename
-                                DetailedChangesetsRequest detailedRequest = new DetailedChangesetsRequest.Builder(repository)
-                                .changesetId(changesetId)
-                                .ignoreMissing(false)
-                                .maxChangesPerCommit(9999)
-                                .build();
-
-                                log.info("DetailedRequest finished building");
-
-                                Page<DetailedChangeset> detailPage = null;
-
-                                // Setup paging for request even though we only want 1
-                                PageRequest pageRequestDetail = PageUtils.newRequest(0, 25);
-
-                                detailPage = commitService.getDetailedChangesets(detailedRequest, pageRequestDetail);
-
-                                log.info("getDetailedChangesets finished");
-                                String fileName = "";
-                               
-                                // Getting the change and finding the path
-                                for (DetailedChangeset detailedChangeset : detailPage.getValues()){
-                                    log.info("found detailedChangeset");
-                                    for(Change change : detailedChangeset.getChanges().getValues()){
-                                        log.info("found change");
-
-                                        if(fileName.isEmpty()) {
-                                            fileName = change.getPath().toString();
-                                        }
-                                        else {
-                                            fileName = "-" + fileName + change.getPath().toString();
-                                        }
-                                    }
-                                }
-
-                                log.info("FileName {}", fileName);
-
-                                //# Build the FogBugz URI
-                                String r1 = "hp=" + parentID + ";hpb=" + parentID;
-                                String r2 = "h=" + currentID + ";hb="+ parentID;
-                                url = url + "/cvsSubmit.asp?ixBug=" + bugzID + "&sFile=" + fileName + "&sPrev="+ r1 +"&sNew=" + r2 + "&sRepo=" + repository;
-                                new URL(url).openConnection().getInputStream().close();
-                            }
+                            notifyFogbugz(parentID, currentID, bugzID, fileName, repository, url);
+                            log.info("Fogbugz Notified {}", url);
                         }
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-            }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.info("ERROR {}", e.getMessage());
         }
     }
 
@@ -178,5 +118,89 @@ public class FogbugzHook implements AsyncPostReceiveRepositoryHook, RepositorySe
          if (settings.getString("bugzidregex", "").isEmpty()) {
             errors.addFieldError("bugzidregex", "Regex field is blank, please supply one");
         }
+    }
+
+    // Notify Fogbugz URL
+    private void notifyFogbugz(String parentID, String currentID, String bugzID, String fileName, Repository repository, String url){
+        try{
+                 //# Build the FogBugz URI
+                String r1 = "hp=" + parentID + ";hpb=" + parentID;
+                String r2 = "h=" + currentID + ";hb="+ parentID;
+                url = url + "/cvsSubmit.asp?ixBug=" + bugzID + "&sFile=" + fileName + "&sPrev="+ r1 +"&sNew=" + r2 + "&sRepo=" + repository;
+                new URL(url).openConnection().getInputStream().close();
+                log.info("Fogbugz Notified {}", url);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                log.info("ERROR {}", e.getMessage());
+            }
+    }
+
+    // Create detailed request to get filename
+    private String getFileName(String changesetId, Repository repository){
+        DetailedChangesetsRequest detailedRequest = new DetailedChangesetsRequest.Builder(repository)
+        .changesetId(changesetId)
+        .ignoreMissing(false)
+        .maxChangesPerCommit(9999)
+        .build();
+
+        log.info("DetailedRequest finished building");
+
+        Page<DetailedChangeset> detailPage = null;
+
+        // Setup paging for request even though we only want 1
+        PageRequest pageRequestDetail = PageUtils.newRequest(0, 25);
+
+        detailPage = commitService.getDetailedChangesets(detailedRequest, pageRequestDetail);
+
+        log.info("getDetailedChangesets finished");
+        String fileName = "";
+                               
+        // Getting the change and finding the path
+        for (DetailedChangeset detailedChangeset : detailPage.getValues()){
+            log.info("found detailedChangeset");
+            for(Change change : detailedChangeset.getChanges().getValues()){
+                log.info("found change");
+
+                if(fileName.isEmpty()) {
+                    fileName = change.getPath().toString();
+                }
+                else {
+                    fileName = "-" + fileName + change.getPath().toString();
+                }
+            }
+        }
+
+        return fileName;
+    }
+
+    // For each RefChange, use the fromHash and toHash to extract the individual changesets: 
+    // https://developer.atlassian.com/static/javadoc/stash/latest/api/reference/com/atlassian/stash/repository/RefChange.html
+    private Collection<Changeset> extractChangesets(String fromHash, String toHash, Repository repository) {
+        ChangesetsBetweenRequest request = new ChangesetsBetweenRequest.Builder(repository)
+        .exclude(fromHash)
+        .include(toHash)
+        .build();
+
+        // Setup paging for request
+        PageRequest page_request = PageUtils.newRequest(0, 25);
+
+        Collection<Changeset> changesetsForRefChange = Lists.newArrayList();
+        Page<Changeset> page = null;
+
+        // page through change sets and add to list
+        while (page == null || !page.getIsLastPage()) {
+
+            // Using CommitService.getChangesetsBetween get a list of changesets from RefChange.getFromHash and RefChange.getToHash
+            page = commitService.getChangesetsBetween(request, page_request);
+
+            for (Changeset changeset : page.getValues()){
+                changesetsForRefChange.add(changeset);
+            }
+
+            page_request = page.getNextPageRequest();
+        }
+
+        return changesetsForRefChange;
     }
 }
